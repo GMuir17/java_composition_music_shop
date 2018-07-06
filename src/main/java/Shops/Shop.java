abf5cbc62304f58ef011fb6f584c18c27cc506e3
Gary Muir <garymuir17@gmail.com>
2018-07-06 13:04:24 +0100

    sets up the Shop class

--- src/main/java/Shops/Shop.java
+++ src/main/java/Shops/Shop.java
@@ -1,4 +1,25 @@
 package Shops;
 
+import Behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList getStock() {
+        return this.stock;
+    }
+
 }
--- src/test/java/Shops/ShopTest.java
+++ src/test/java/Shops/ShopTest.java
@@ -1,4 +1,30 @@
 package Shops;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void setUp() {
+        this.shop = new Shop("Jimmy Egypt's");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jimmy Egypt's", shop.getName());
+    }
+
+    @Test
+    public void hasEmptyStock() {
+        ArrayList emptyStock = shop.getStock();
+        assertEquals(0, emptyStock.size());
+    }
+
 }
