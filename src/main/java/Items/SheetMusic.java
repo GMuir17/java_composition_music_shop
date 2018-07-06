d8c6f887f1f43c867c074106d483a09b07ee42ca
Gary Muir <garymuir17@gmail.com>
2018-07-06 12:54:01 +0100

    sets up SheetMusic

--- src/main/java/Items/SheetMusic.java
+++ src/main/java/Items/SheetMusic.java
@@ -1,4 +1,32 @@
 package Items;
 
 public class SheetMusic {
+
+    private String description;
+    private int buyingPrice;
+    private int sellingPrice;
+
+    public SheetMusic(String description, int buyingPrice, int sellingPrice) {
+        this.description = description;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getBuyingPrice() {
+        return this.buyingPrice;
+    }
+
+    public int getSellingPrice() {
+        return this.sellingPrice;
+    }
+
+    public int calculateMarkup() {
+        int markup = this.sellingPrice - this.buyingPrice;
+        return markup;
+    }
+
 }
--- src/test/java/Items/SheetMusicTest.java
+++ src/test/java/Items/SheetMusicTest.java
@@ -1,4 +1,38 @@
 package Items;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
+
+    SheetMusic sheetMusic;
+
+    @Before
+    public void setUp() {
+        this.sheetMusic = new SheetMusic("Chopin", 10, 50);
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("Chopin", sheetMusic.getDescription());
+    }
+
+    @Test
+    public void hasBuyingPrice() {
+        assertEquals(10, sheetMusic.getBuyingPrice());
+    }
+
+    @Test
+    public void hasSellingPrice() {
+        assertEquals(50, sheetMusic.getSellingPrice());
+    }
+
+    @Test
+    public void canCalculateMarkup() {
+        assertEquals(40, sheetMusic.calculateMarkup());
+    }
+
+
 }
